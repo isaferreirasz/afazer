acda6aefd79f5e351e30cf473ac08cc51cc96bf5
Isabele Ferreira <isabele.ferreira.707@ufrn.edu.br>
2025-05-04 18:54:09 +0000

    teste adicionar nova tarefa

---
+++ script.js
@@ -0,0 +1,14 @@
+function adicionarTarefa(){
+    const titulo_tarefa = document.getElementById('titulo-tarefa').value;
+    const data_tarefa = document.getElementById('data-tarefa').value;
+    const urgencia_tarefa = document.getElementById('urgencia-tarefa').value;
+
+    const novaTarefa = {
+        titulo_tarefa,
+        data_tarefa,
+        urgencia_tarefa,
+    }
+}
+
+console.log(novaTarefa)
+
