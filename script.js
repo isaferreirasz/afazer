df048831ada8e2927c31dfed40706fa0fefe20b4
Isabele Ferreira <isabele.ferreira.707@ufrn.edu.br>
2025-05-05 18:57:55 +0000

    adicionar tarefa funcionando

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function adicionarTarefa(){
+const btn = document.querySelector('#enviar')
+const tarefas = document.querySelector('#tarefas')
+btn.addEventListener("click", function(event){
+    event.preventDefault();
     const titulo_tarefa = document.getElementById('titulo-tarefa').value;
     const data_tarefa = document.getElementById('data-tarefa').value;
     const urgencia_tarefa = document.getElementById('urgencia-tarefa').value;
@@ -8,8 +11,7 @@ function adicionarTarefa(){
         data_tarefa,
         urgencia_tarefa,
     }
+tarefas.innerHTML += `Título: ${novaTarefa.titulo_tarefa}, Data: ${novaTarefa.data_tarefa}, Urgência: ${novaTarefa.urgencia_tarefa}`
+window.alert("Tarefa adicionada")
 }
-
-console.log(novaTarefa.titulo_tarefa)
-console.log(novaTarefa.data_tarefa)
-console.log(novaTarefa.urgencia_tarefa)
+)
